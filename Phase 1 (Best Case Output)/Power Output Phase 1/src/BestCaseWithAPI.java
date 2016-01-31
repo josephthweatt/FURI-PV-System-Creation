b75362a2f46f9d05c9090c48eb28a407b00e237b
JOSEPH_THWEATT <josephthweatt@gmail.com>
2016-01-30 22:38:11 -0700

    Testing average daylight hours hypothesis
    The tests here show that the average daylight hours of a given location
    on earth comes out to around 12 hours per day. With the latitude and
    longitude provided, the code found there to be 4446.133 hours of
    sunlight for the year, or about 12.14 hours on average per day. I'll use
    this as proof of the 12-hour theory and continue to the next step of
    this research. If I get more time near the end of the research, I will
    aim to calculate the real amount of sunlight per day.

--- Phase 1 (Best Case Output)/Power Output Phase 1/src/BestCaseWithAPI.java
+++ Phase 1 (Best Case Output)/Power Output Phase 1/src/BestCaseWithAPI.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.HashMap;
 
 import org.apache.commons.io.IOUtils;
 import org.json.JSONException;
@@ -17,6 +18,11 @@ public class BestCaseWithAPI {
 	private static String sunrise, sunset;
 	private static double sunriseDbl, sunsetDbl;
 	private static double daylightHours;
+	
+	//vars to find the average daylight hours
+	private static double totalDaylight;
+	private static int year;
+	private static int[] daysInMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
 
 	public static void main(String[] args) throws IOException, JSONException {
 		getDaylightHours();
@@ -27,23 +33,47 @@ public class BestCaseWithAPI {
 	public static void getDaylightHours() throws IOException, JSONException {
 		latitude = "lat=" + 33.45; //az lat
 		longitude = "lng=" + 112.0667; //az long
-		String url = apiSite + format + latitude + "&"+ longitude;
-		String source = IOUtils.toString(new URL(url), Charset.forName("UTF-8"));
-		JSONObject mainObject = new JSONObject(source);
-		mainObject = mainObject.getJSONObject(resultKey);
-		sunrise = mainObject.getString(sunriseKey);
-		sunset = mainObject.getString(sunsetKey);
 		
-		String[] dubs = sunrise.split(":");	
-		sunriseDbl = Double.parseDouble(dubs[0]) + Double.parseDouble(dubs[1]) / 60;
-		
-		dubs = sunset.split(":");
-		sunsetDbl = Double.parseDouble(dubs[0]) + Double.parseDouble(dubs[1]) / 60;
-		if (sunsetDbl < 12) {
-			sunsetDbl += 12;
+		//material used for getting the date
+		year = 2016; //lets just assume this, I'll make it changeable if the concept works.
+		if ((year % 4 == 0) && (year % 100 == year % 400)) {
+			daysInMonth[1]++;
 		}
 		
-		daylightHours = sunsetDbl - sunriseDbl;
+		//now the calls
+		for (int i = 1; i <= 365; i++) {
+			date = "date=" + getDayOfYear(i);
+			String url = apiSite + format + latitude + "&"+ longitude + "&" + date;
+			String source = IOUtils.toString(new URL(url), Charset.forName("UTF-8"));
+			JSONObject mainObject = new JSONObject(source);
+			mainObject = mainObject.getJSONObject(resultKey);
+			sunrise = mainObject.getString(sunriseKey);
+			sunset = mainObject.getString(sunsetKey);
+			
+			String[] dubs = sunrise.split(":");	
+			sunriseDbl = Double.parseDouble(dubs[0]) + Double.parseDouble(dubs[1]) / 60;
+			
+			dubs = sunset.split(":");
+			sunsetDbl = Double.parseDouble(dubs[0]) + Double.parseDouble(dubs[1]) / 60;
+			if (sunsetDbl < 12) {
+				sunsetDbl += 12;
+			}
+			
+			daylightHours = sunsetDbl - sunriseDbl;
+			totalDaylight += daylightHours;
+			System.out.println("finished " + date);
+		}
+		System.out.println(totalDaylight);
 	}
-	
+		
+	public static String getDayOfYear(int day) {
+		for (int i = 0; i < 12; i++) {
+			if (day - daysInMonth[i] >= 0) {
+				day -= daysInMonth[i];
+			} else {
+				return year + "-" + (i + 1) + "-" + day;
+			}
+		}
+		return year + "-" + 12 + "-00"; //for december
+	}
 }
