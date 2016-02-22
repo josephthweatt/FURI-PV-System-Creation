c00f3066af93294f91a7bd06df049315876ff4d3
JOSEPH_THWEATT <josephthweatt@gmail.com>
2016-02-21 22:59:10 -0700

    Created System Creator Subclass
    SystemCreator Subclass was created in System Manager. It is a public
    variable and can be accessed through its parent class. SysCreator is
    currently the class holding the ProductContainers, but that might change
    to SystemManager if it becomes too restrictive

--- EclipseWorkBench/src/SystemManager.java
+++ EclipseWorkBench/src/SystemManager.java
@@ -3,9 +3,11 @@ import java.util.HashMap;
 import ProductObjects.FullSystem;
 
 public class SystemManager {
+	public SystemCreator systemCreator;
 	private HashMap<String, FullSystem> systemMap;
 
 	public SystemManager() {
+		systemCreator = new SystemCreator();
 		systemMap = new HashMap<String, FullSystem>();
 	}
 
@@ -13,14 +15,24 @@ public class SystemManager {
 		return systemMap.get(systemName);
 	}
 
-	// grabs products and inserts them into a full System (products must
-	// already be made as objects). Will also return a system if it is needed
-	public FullSystem commitProductsToSystem(String systemName,
-			Object... product) {
-		systemMap.put(systemName, new FullSystem());
-		for (int i = 0; i < product.length; i++) {
-			systemMap.get(systemName).addProduct(product);
+	// this subclass is dedicated to creating new PVSystems
+	public class SystemCreator {
+		public ProductsContainer[] containers;
+		
+		public SystemCreator() {
+			containers = new ProductsContainer[9];
 		}
-		return systemMap.get(systemName);
+		
+		// grabs products and inserts them into a full System (products must
+		// already be made as objects). Will also return a system if it is needed
+		public FullSystem commitProductsToSystem(String systemName,
+				Object... product) {
+			systemMap.put(systemName, new FullSystem());
+			for (int i = 0; i < product.length; i++) {
+				systemMap.get(systemName).addProduct(product);
+			}
+			return systemMap.get(systemName);
+		}
+
 	}
 }
