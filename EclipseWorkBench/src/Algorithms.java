482b735ac0bfae25b2d534d1fd4b120159f2610a
JOSEPH_THWEATT <josephthweatt@gmail.com>
2016-03-17 21:01:00 -0700

    Created methods to find viable products in the databases
    Some of these may be taken out later, I just wanted them here now to
    show the intent of the class

--- EclipseWorkBench/src/Algorithms.java
+++ EclipseWorkBench/src/Algorithms.java
@@ -10,9 +10,9 @@ public class Algorithms {
 	public double energyInVolts; // an optional field. I may delete it later
 
 	/*
-	 * ViableProductLists will contain all products that have the potential to be
-	 * used in a system, it will be used to experiment with viable systems and
-	 * pass them into the systemCreator
+	 * ViableProductLists will contain all products that have the potential to
+	 * be used in a system, it will be used to experiment with viable systems
+	 * and pass them into the systemCreator
 	 */
 	public ArrayList<Panel> viablePanels;
 	public ArrayList<Inverter> viableInverters;
@@ -23,7 +23,7 @@ public class Algorithms {
 	public ArrayList<DCACDisconnect> viableDCACDisconnects;
 	public ArrayList<BatteryWire> viableBatteryWires;
 	public ArrayList<PVWire> viablePVWires;
-	
+
 	public Algorithms(double budget, double energyInWatts,
 			double availableSpace) {
 		this.budget = budget;
@@ -38,6 +38,51 @@ public class Algorithms {
 		this.energyInWatts = energyInWatts;
 		this.availableSpace = availableSpace;
 		this.energyInVolts = energyInVolts;
+	}
+
+	// looks for panels that might work in the system
+	public void findViablePanels() {
+
+	}
+
+	// looks for inverters that might work in the system
+	public void findViableInverters() {
+
+	}
+
+	// looks for racks that might work in the system
+	public void findViableRacks() {
+
+	}
+
+	// looks for batteries that might work in the system
+	public void findViableBatteries() {
+
+	}
+
+	// looks for battery controllers that might work in the system
+	public void findViableBatteryControllers() {
+
+	}
+
+	// looks for battery meters that might work in the system
+	public void findViableBatteryMeters() {
+
+	}
+
+	// looks for DCACDisconnect that might work in the system
+	public void findViableDCACDisconnet() {
+
+	}
+
+	// looks for battery wires that might work in the system
+	public void findViableBatteryWires() {
+
+	}
+
+	// looks for PVWires that might work in the system
+	public void findViablePVWires() {
+
 	}
 
 }
